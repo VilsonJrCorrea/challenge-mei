e02a1c875f3151e827bcc5b5faf934b425bfc843
Vilson Junior <vilsonjrcorrea@gmail.com>
2019-06-23 20:32:22 -0300

    update components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,26 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
 
 const express = require('express');
 const cors = require('cors');
 
-const app = express();
-app.use(cors());
-app.options('*', cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+class AppController {
+  constructor() {
+    this.express = express();
+    this.middlewares();
+  }
 
-app.use('/api/accounts', require('./account/accountRoutes'));
-app.use('/api/payments', require('../components/payment/paymentRoutes'));
-app.use('/api/releases', require('../components/release/releaseRoutes'));
+  middlewares() {
+    this.express.use(express.json());
+    this.express.use(cors());
+    this.express.options('*', cors());
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: true }));
+    this.express.use('/api/accounts', require('../components/account/accountRoutes'));
+    this.express.use('/api/payments', require('../components/payment/paymentRoutes'));
+    this.express.use('/api/releases', require('../components/release/releaseRoutes'));
+  }
+}
 
-module.exports = { app };
+module.exports = new AppController().express;
